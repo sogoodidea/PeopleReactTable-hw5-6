4e5e3448ab0b64b879a2bdb0c471dd73cd00de41
Tzirel Leah <tlgoodidea@gmail.com>
2020-09-16 17:10:20 -0400

    Fixed table-danger

--- hw 5-6 ReactPeopleTable/ClientApp/src/PersonRow.js
+++ hw 5-6 ReactPeopleTable/ClientApp/src/PersonRow.js
@@ -4,7 +4,7 @@ class PersonRow extends React.Component {
 
     render() {
         return (
-            <tr className={this.props.person.age >= 65 ? 'danger' : ''}>
+            <tr className={this.props.person.age >= 65 ? 'table-danger' : ''}>
                 <td>{this.props.person.firstName}</td>
                 <td>{this.props.person.lastName}</td>
                 <td>{this.props.person.age}</td>
